710553efa4b5740da972c28f636ad136d3afc1ad
Damir <damir@withpulp.com>
2015-05-22 22:10:26 -0400

    added routes and templates

--- router/routes.js
+++ router/routes.js
@@ -3,3 +3,27 @@ Router.route('/', {
 }, function () {
   SEO.set({ title: Meteor.App.NAME });
 });
+
+Router.route('/about', {
+	name: 'about'
+}, function () {
+  SEO.set({ title: Meteor.App.NAME });
+});
+
+Router.route('/services', {
+	name: 'services'
+}, function () {
+  SEO.set({ title: Meteor.App.NAME });
+});
+
+Router.route('/tools', {
+	name: 'tools'
+}, function () {
+  SEO.set({ title: Meteor.App.NAME });
+});
+
+Router.route('/clients', {
+	name: 'clients'
+}, function () {
+  SEO.set({ title: Meteor.App.NAME });
+});
