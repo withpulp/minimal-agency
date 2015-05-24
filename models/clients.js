ae290b7125c1278b54e11f9c8338226a2b79fd0f
Damir <damir@withpulp.com>
2015-05-24 17:28:12 -0400

    added allow and helpers to clients and projects models

--- models/clients.js
+++ models/clients.js
@@ -1,1 +1,20 @@
 Clients = new Mongo.Collection('clients');
+
+Clients.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Clients.helpers({
+  clients: function() {
+    return Clients.find();
+  }
+});
--- models/projects.js
+++ models/projects.js
@@ -1,1 +1,20 @@
 Projects = new Mongo.Collection('projects');
+
+Projects.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Projects.helpers({
+  projects: function() {
+    return Projects.find();
+  }
+});
