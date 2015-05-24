1b63ea505dbbf740ca4be25bf9c0373ea9d521d5
Husam Machlovi <hmachlovi@gmail.com>
2015-05-24 17:44:04 -0400

    created services collection and helper

---
+++ models/services.js
@@ -0,0 +1,20 @@
+Services = new Mongo.Collection('services');
+
+Services.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Services.helpers({
+  projects: function() {
+    return Services.find();
+  }
+});
