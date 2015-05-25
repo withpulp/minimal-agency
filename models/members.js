a9f74a54f67ea7144296fefccc10c721054673ca
Damir <damir@withpulp.com>
2015-05-24 23:27:26 -0400

    added members model

---
+++ models/members.js
@@ -0,0 +1,20 @@
+Members = new Mongo.Collection('members');
+
+Members.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Members.helpers({
+  members: function() {
+    return Members.find();
+  }
+});
