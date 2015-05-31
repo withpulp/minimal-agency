d24797e31aaf1269056656b902e6e1d5fa6c3a9c
Husam Machlovi <hmachlovi@gmail.com>
2015-05-31 13:30:10 -0400

    root schema - wip

---
+++ server/fixtures/root.js
@@ -0,0 +1,80 @@
+// The "||" notation doesn't work yet
+Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
+
+Fixtures.root = [
+  {
+    mission: '/images/clients/logo.png',
+    sections: [
+      {
+        header: 'benefits header',
+        text: [
+            {
+              text: 'test'
+            },
+            {
+              text: 'test',
+            },
+            {
+              text: 'test',
+            }
+        ]
+      },
+      {
+        header: 'clients header',
+        text: [
+            {
+              text: 'test'
+            },
+            {
+              text: 'test',
+            },
+            {
+              text: 'test',
+            }
+        ]
+      },
+      {
+        header: 'services header',
+        text: [
+            {
+              text: 'test'
+            },
+            {
+              text: 'test',
+            },
+            {
+              text: 'test',
+            }
+        ]
+      },
+      {
+        header: 'process header',
+        text: [
+            {
+              text: 'test'
+            },
+            {
+              text: 'test',
+            },
+            {
+              text: 'test',
+            }
+        ]
+      },
+      {
+        header: 'tools header',
+        text: [
+            {
+              text: 'test'
+            },
+            {
+              text: 'test',
+            },
+            {
+              text: 'test',
+            }
+        ]
+      }
+    ]
+  }
+]
