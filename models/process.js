6e7db45bb876b282dc0301b2b80b7a511cd8d6e2
Husam Machlovi <hmachlovi@gmail.com>
2015-06-01 19:16:51 -0400

    process collection

---
+++ models/process.js
@@ -0,0 +1,20 @@
+Process = new Mongo.Collection('process');
+
+Process.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Process.helpers({
+  members: function() {
+    return Process.find();
+  }
+});
