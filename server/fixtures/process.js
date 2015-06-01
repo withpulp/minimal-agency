22f6869c79e9ba4c047e7bc29ca3cc850ad75650
Husam Machlovi <hmachlovi@gmail.com>
2015-06-01 19:22:23 -0400

    process fixtures schema setup and content authoring - wip

---
+++ server/fixtures/process.js
@@ -0,0 +1,18 @@
+// The "||" notation doesn't work yet
+Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
+
+Fixtures.process = [
+  {
+    header: 'We take an iterative approach to design and development that empowers minimal time to market.',
+    sections: [
+      {
+        statement: 'Measuring the Forces',
+        description: 'We define the job that a user needs to get done by analyzing the qualitative and quantitive datasets surrounding the job. These datasets are the forces that bear down on the problem. They provide the context for the form the product will take.'
+      },
+      {
+        statement: 'Outcome Definition',
+        description: ''
+      }
+        ]
+  }
+]
