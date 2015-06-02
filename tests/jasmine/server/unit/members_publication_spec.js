08faf85020b4bd69b4ca6df9096cb2a35a901cc4
Damir <damir@withpulp.com>
2015-06-02 16:38:37 -0400

    added todo to members_publication_spec

--- tests/jasmine/server/unit/members_publication_spec.js
+++ tests/jasmine/server/unit/members_publication_spec.js
@@ -4,5 +4,6 @@ describe('Members publications', function () {
 
   it('returns the entire members collection to the client', function () {
 
+    // @TODO: test for the publish all function
   });
 });
