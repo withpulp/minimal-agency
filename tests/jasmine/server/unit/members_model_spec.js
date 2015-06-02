36d37b108ed6b8ab416aecc001c96d55b4142b0e
Damir <damir@withpulp.com>
2015-06-02 16:37:24 -0400

    added server unit tests for members model and publication

---
+++ tests/jasmine/server/unit/members_model_spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Members model', function() {
+
+  it('is a Mongo collection of the agency\'s members', function() {
+
+    // @TODO: tests for allow and helper functions
+  });
+});
---
+++ tests/jasmine/server/unit/members_publication_spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Members publications', function () {
+
+  it('returns the entire members collection to the client', function () {
+
+  });
+});
