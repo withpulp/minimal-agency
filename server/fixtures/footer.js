e70203fc365499d52b58b922d974927a3eb8f52b
Husam Machlovi <hmachlovi@gmail.com>
2015-06-09 16:24:08 -0400

    footer fixture schema and content loading

--- server/fixtures/footer.js
+++ server/fixtures/footer.js
@@ -2,4 +2,18 @@
 Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
 
 Fixtures.footer = [
+  {
+    title: 'contact',
+    list: [
+      {
+        item: 'newbiz@utool.com'
+      },
+      {
+        item: 'careers@utool.com'
+      },
+      {
+        item: 'press@utool.com'
+      }
+    ]
+  }
 ];
