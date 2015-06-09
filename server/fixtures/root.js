1a5c61ace8f3297f9362b5678968cda0ef917539
Damir <damir@withpulp.com>
2015-06-09 14:31:23 -0400

    added each loop into root - wip

--- server/fixtures/root.js
+++ server/fixtures/root.js
@@ -10,81 +10,81 @@ Fixtures.root = [
         id: 'benefits',
         statement: 'Attract more customers with:',
         points: [
-            {
-              text: 'Growth Opportunities'
-            },
-            {
-              text: 'Trial Signups',
-            },
-            {
-              text: 'Leads',
-            },
-            {
-              text: 'Sales and Purchases',
-            },
-            {
-              text: 'Click-throughs',
-            }
+          {
+            text: 'Growth Opportunities'
+          },
+          {
+            text: 'Trial Signups',
+          },
+          {
+            text: 'Leads',
+          },
+          {
+            text: 'Sales and Purchases',
+          },
+          {
+            text: 'Click-throughs',
+          }
         ]
       },
       {
         id: 'figures',
         statement: 'Here is our recent track record',
         points: [
-            {
-              date: '2015 May',
-              metric: 'Purchases',
-              percent: '25%',
-              client: 'Wholesale E-commerce Company'
-            },
-            {
-              date: '2015 May',
-              metric: 'Signups',
-              percent: '30%',
-              client: 'Wholesale E-commerce Company'
-            },
-            {
-              date: '2015 March',
-              metric: 'Signups',
-              percent: '40%',
-              client: 'Yoga Instructor'
-            },
-            {
-              date: '2015 February',
-              metric: 'Signups',
-              percent: '40%',
-              client: 'Product Strategy Consultant'
-            },
-            {
-              date: '2015 January',
-              metric: 'Donations',
-              percent: '15%',
-              client: 'Non-profit Organization'
-            }
+          {
+            date: '2015 May',
+            metric: 'Purchases',
+            percent: '25%',
+            client: 'Wholesale E-commerce Company'
+          },
+          {
+            date: '2015 May',
+            metric: 'Signups',
+            percent: '30%',
+            client: 'Wholesale E-commerce Company'
+          },
+          {
+            date: '2015 March',
+            metric: 'Signups',
+            percent: '40%',
+            client: 'Yoga Instructor'
+          },
+          {
+            date: '2015 February',
+            metric: 'Signups',
+            percent: '40%',
+            client: 'Product Strategy Consultant'
+          },
+          {
+            date: '2015 January',
+            metric: 'Donations',
+            percent: '15%',
+            client: 'Non-profit Organization'
+          }
         ]
       },
       {
         id: 'clients',
-        statement: 'We\ve worked with:',
+        statement: 'We\'ve worked with:',
         clients: [
-            {
-              thumb: 'images/clients/pfizer.png'
-            },
-            {
-              thumb: 'images/clients/jj.png'
-            },
-            {
-              thumb: 'images/clients/sva.png'
-            },
-            {
-              thumb: 'images/clients/thermofisher.png'
-            },
-            {
-              thumb: 'images/clients/amc.png'
-            },
-            {
-              thumb: 'images/clients/novo.png'
-            }
+          {
+            thumb: 'images/clients/pfizer.png'
+          },
+          {
+            thumb: 'images/clients/jj.png'
+          },
+          {
+            thumb: 'images/clients/sva.png'
+          },
+          {
+            thumb: 'images/clients/thermofisher.png'
+          },
+          {
+            thumb: 'images/clients/amc.png'
+          },
+          {
+            thumb: 'images/clients/novo.png'
+          }
         ]
       },
       {
@@ -106,14 +106,14 @@ Fixtures.root = [
         id: 'tools',
         statement: 'Recent products:',
         points: [
-            {
-              title: 'Landing Page Builder',
-              description: 'Create an effective landing page in minutes. Perfect for coming soon and newly launched products.'
-            },
-            {
-              title: 'Blog',
-              description: 'Minimal blogging platform designed for readability.'
-            }
+          {
+            title: 'Landing Page Builder',
+            description: 'Create an effective landing page in minutes. Perfect for coming soon and newly launched products.'
+          },
+          {
+            title: 'Blog',
+            description: 'Minimal blogging platform designed for readability.'
+          }
         ]
       }
     ]
