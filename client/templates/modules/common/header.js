02bf8dfb319ab642e94faae82f3b2e9ac789fe44
Damir <damir@withpulp.com>
2015-06-10 12:37:00 -0400

    updates to logo scroll feature

--- client/templates/modules/common/header.js
+++ client/templates/modules/common/header.js
@@ -1,40 +1,40 @@
 Template.header.rendered = function () {
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = -250;
-var navbarHeight = $('header').outerHeight();
+  // Hide logo on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 0;
+  var navbarHeight = $('.logo').outerHeight();
 
-$(window).scroll(function(event){
+  $(window).scroll(function(event){
     didScroll = true;
-});
+  });
 
-setInterval(function() {
+  setInterval(function() {
     if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+      hasScrolled();
+      didScroll = false;
     }
-}, 0);
+  }, 0);
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
+  function hasScrolled() {
+    var scroll = $(this).scrollTop();
 
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
+    if (Math.abs(lastScrollTop - scroll) <= delta)
+      return;
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
+    if (scroll > lastScrollTop && scroll > navbarHeight){
+      // Scroll Down
+      $('header').removeClass('down').addClass('up');
     } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
+      // Scroll Up
+      if (scroll + $(window).height() < $(document).height()) {
+        $('header').removeClass('up').addClass('down');
+      }
     }
 
-    lastScrollTop = st;
-}
+    lastScrollTop = scroll;
+  }
 }
