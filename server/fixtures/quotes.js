69bd02c73804d53833e94219130d7d68a7668250
Husam Machlovi <hmachlovi@gmail.com>
2015-06-18 13:58:13 -0400

    added quotes

--- server/fixtures/quotes.js
+++ server/fixtures/quotes.js
@@ -2,4 +2,20 @@
 Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
 
 Fixtures.quotes = [
+  {
+    author: 'Peter Drucker',
+    quote: 'What gets measured gets managed.'
+  },
+  {
+    author: 'Tim O\'Reilly',
+    quote: 'We\'re entering a new world in which data may be more important than software.'
+  },
+  {
+    author: 'Albert Einstein',
+    quote: 'Not everything that can be counted counts,  and not everything that counts can be counted.'
+  },
+  {
+    author: 'Albert Einstein',
+    quote: 'As far as the laws of mathematics refer to reality, they are not certain; and as far as they are certain, they do not refer to reality.'
+  }
 ];
