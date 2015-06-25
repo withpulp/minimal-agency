54ecc02eb9320d3dd1b936269f22c8d60bb6cfc6
Damir <damir@withpulp.com>
2015-06-24 18:47:56 -0700

    test bs

--- tests/jasmine/server/unit/package-stubs.js
+++ tests/jasmine/server/unit/package-stubs.js
@@ -7209,7 +7209,7 @@ var packageMetadata = {
             },
             "connections": {
               "type": "constant",
-              "value": 0
+              "value": 1
             },
             "timeout": {
               "type": "constant",
--- tests/jasmine/server/unit/packageMocksSpec.js
+++ tests/jasmine/server/unit/packageMocksSpec.js
@@ -7210,7 +7210,7 @@ var packageMetadata = {
             },
             "connections": {
               "type": "constant",
-              "value": 0
+              "value": 1
             },
             "timeout": {
               "type": "constant",
