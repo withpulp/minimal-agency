aa002358f38b374d5a8f6fbdb4602d8366f78b93
Damir <damir@withpulp.com>
2015-07-01 11:59:24 -0400

    tests

--- tests/jasmine/server/unit/packageMocksSpec.js
+++ tests/jasmine/server/unit/packageMocksSpec.js
@@ -7210,7 +7210,7 @@ var packageMetadata = {
             },
             "connections": {
               "type": "constant",
-              "value": 6
+              "value": 0
             },
             "timeout": {
               "type": "constant",
