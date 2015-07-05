c2f89e49063b1370b6b9c24f3defaa8b178807f6
Damir <damir@withpulp.com>
2015-07-05 00:53:20 -0400

    created toolbar module

--- client/lib/spacebar_helpers.js
+++ client/lib/spacebar_helpers.js
@@ -1,3 +1,7 @@
 Template.registerHelper('menuToggle', function() {
   return Session.get('menuToggle') ? 'navigator' : '';
 });
+
+Template.registerHelper('admin', function() {
+  return Session.get('admin');
+});
