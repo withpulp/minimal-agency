2aadb3eebb410bbe795f8543977c7ec69e52d788
Damir <damir@withpulp.com>
2015-07-05 00:59:31 -0400

    removed admin helper

--- client/lib/spacebar_helpers.js
+++ client/lib/spacebar_helpers.js
@@ -1,7 +1,3 @@
 Template.registerHelper('menuToggle', function() {
   return Session.get('menuToggle') ? 'navigator' : '';
 });
-
-Template.registerHelper('admin', function() {
-  return Session.get('admin');
-});
