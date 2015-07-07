03e7b7d98f7bd370597d2a8a60ed9cd5f4d8971e
Damir <damir@withpulp.com>
2015-07-07 12:58:15 -0400

    added more publications to posts for admin dashboard

--- server/publications/posts.js
+++ server/publications/posts.js
@@ -1,3 +1,36 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+Meteor.publish('postsWithUsers', {
+  find: function() {
+    return Posts.find();
+  },
+  children: [{
+    find: function(post) {
+      return Meteor.users.find({
+        _id: post.createdBy
+      }, {
+        fields: { profile: 1 }
+      });
+    }
+  }]
+});
+
+Meteor.publish('postWithUser', function(postId) {
+  check(postId, String);
+  return {
+    find: function() {
+      return Posts.find({ _id: postId });
+    },
+    children: [{
+      find: function(post) {
+        return Meteor.users.find({
+          _id: post.createdBy
+        }, {
+          fields: { profile: 1 }
+        });
+      }
+    }]
+  }
+});
