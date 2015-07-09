207795cf86568e7337684ebafa2489f120cdf870
Damir <damir@withpulp.com>
2015-07-09 12:50:33 -0400

    updated footer template spec

--- tests/jasmine/client/integration/footer_template_spec.js
+++ tests/jasmine/client/integration/footer_template_spec.js
@@ -2,13 +2,31 @@
 
 describe('Footer template', function() {
 
-	it('should show a contact list module', function() {
+	it('should show a mailchimp subscription module', function() {
 		var div = document.createElement('div'),
 		    comp = UI.render(Template.footer);
 
 		UI.insert(comp, div);
 
-		expect($(div).find('.contact.module')[0]).toBeDefined();
+		expect($(div).find('.mailchimp.module')[0]).toBeDefined();
+	});
+
+	it('should show a quotes module', function() {
+		var div = document.createElement('div'),
+		    comp = UI.render(Template.footer);
+
+		UI.insert(comp, div);
+
+		expect($(div).find('.quotes.module')[0]).toBeDefined();
+	});
+
+	it('should show a contacts list module', function() {
+		var div = document.createElement('div'),
+		    comp = UI.render(Template.footer);
+
+		UI.insert(comp, div);
+
+		expect($(div).find('.contacts.module')[0]).toBeDefined();
 	});
 
 	it('should show a copyright with attribution', function() {
