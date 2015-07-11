751ccb22991bfa96d6e328c4312adcbf4513e7d9
Damir <damir@withpulp.com>
2015-07-11 00:55:23 -0400

    small updates to load_fixtures

--- server/startup/load_fixtures.js
+++ server/startup/load_fixtures.js
@@ -1,61 +1,60 @@
-function loadFixture(fixtures, collection) {
-  var i;
-
-  for (i = 0; i < fixtures.length; i+= 1) {
-    //collection.remove({ });
-    collection.insert(fixtures[i]);
-  }
-}
-
 Meteor.startup(function () {
-  // @TODO: loop this
 
-  if (Projects.find().count() === 0) {
-    loadFixture(Fixtures['projects'], Projects);
+  function loadFixture(fixtures, collection) {
+    var i;
+
+    for (i = 0; i < fixtures.length; i+= 1) {
+      collection.insert(fixtures[i]);
+    }
   }
+
+  // @TODO: loop this
 
   if (Benefits.find().count() === 0) {
     loadFixture(Fixtures['benefits'], Benefits);
-  }
-
-  if (Figures.find().count() === 0) {
-    loadFixture(Fixtures['figures'], Figures);
   }
 
   if (Clients.find().count() === 0) {
     loadFixture(Fixtures['clients'], Clients);
   }
 
-  if (Members.find().count() === 0) {
-    loadFixture(Fixtures['members'], Members);
+  if (Contacts.find().count() === 0) {
+    loadFixture(Fixtures['contacts'], Contacts);
   }
 
-  if (Tools.find().count() === 0) {
-    loadFixture(Fixtures['tools'], Tools);
+  if (Figures.find().count() === 0) {
+    loadFixture(Fixtures['figures'], Figures);
+  }
+
+  if (Members.find().count() === 0) {
+    loadFixture(Fixtures['members'], Members);
   }
 
   if (Posts.find().count() === 0) {
     loadFixture(Fixtures['posts'], Posts);
   }
 
-  if (Services.find().count() === 0) {
-    loadFixture(Fixtures['services'], Services);
-  }
-
-  if (Values.find().count() === 0) {
-    loadFixture(Fixtures['values'], Values);
+  if (Projects.find().count() === 0) {
+    loadFixture(Fixtures['projects'], Projects);
   }
 
   if (Processes.find().count() === 0) {
     loadFixture(Fixtures['processes'], Processes);
   }
 
-  if (Contacts.find().count() === 0) {
-    loadFixture(Fixtures['contacts'], Contacts);
-  }
-
   if (Quotes.find().count() === 0) {
     loadFixture(Fixtures['quotes'], Quotes);
   }
 
+  if (Services.find().count() === 0) {
+    loadFixture(Fixtures['services'], Services);
+  }
+
+  if (Tools.find().count() === 0) {
+    loadFixture(Fixtures['tools'], Tools);
+  }
+
+  if (Values.find().count() === 0) {
+    loadFixture(Fixtures['values'], Values);
+  }
 });
