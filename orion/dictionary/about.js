35c8cc9e1e9c0c88610110fd834a55a2d5de89fa
Damir <damir@withpulp.com>
2015-09-01 12:59:45 -0400

    centered member images in about - added dict terms for leading statements

--- orion/dictionary/about.js
+++ orion/dictionary/about.js
@@ -17,3 +17,18 @@ orion.dictionary.addDefinition('highlight', 'about', {
     type: String,
     label: 'Highlight'
 });
+
+orion.dictionary.addDefinition('servicesStatement', 'about', {
+    type: String,
+    label: 'Services Leading Statement'
+});
+
+orion.dictionary.addDefinition('membersStatement', 'about', {
+    type: String,
+    label: 'Members Leading Statement'
+});
+
+orion.dictionary.addDefinition('processesStatement', 'about', {
+    type: String,
+    label: 'Processes Leading Statement'
+});
