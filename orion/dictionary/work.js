b4d7f130300bbd96fc4b9ad65d378e0c5329b532
Damir <damir@withpulp.com>
2015-09-02 15:22:55 -0400

    created work dict - added header segment to work view

---
+++ orion/dictionary/work.js
@@ -0,0 +1,19 @@
+orion.dictionary.addDefinition('title', 'work', {
+    type: String,
+    label: 'View Title'
+});
+
+orion.dictionary.addDefinition('mission', 'work', {
+    type: String,
+    label: 'Mission Statement'
+});
+
+orion.dictionary.addDefinition('highlight', 'work', {
+    type: String,
+    label: 'Highlight'
+});
+
+orion.dictionary.addDefinition('casesStatement', 'work', {
+    type: String,
+    label: 'Case Studies Leading Statement'
+});
