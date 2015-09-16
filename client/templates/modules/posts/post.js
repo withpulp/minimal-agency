31ae897d3d18d14b9e16f0c80aa0870d8977a47e
Damir <damir@withpulp.com>
2015-09-16 14:57:46 -0400

    more work on next post helper - data being returned, needs sorting

--- client/templates/modules/posts/post.js
+++ client/templates/modules/posts/post.js
@@ -22,8 +22,8 @@
 //	}
 //}
 
-Template.post.onRendered(function() {
-   Meteor.subscribe('posts');
+Template.post.onRendered(function () {
+    Meteor.subscribe('posts');
 });
 
 Template.post.helpers({
@@ -31,10 +31,35 @@ Template.post.helpers({
         return Posts.findOne(Router.current().params._id);
     },
     next: function () {
-        var current = Router.current().params._id;
-        console.log(current);
-        var next = Posts.find({createdAt: {$gt: current.createdAt}}, {sort: {createdAt: 1}, limit:1});
-        console.log(next);
-        return next;
+        //var current = Router.current().params._id;
+        //console.log(current);
+        //var next = Posts.find({createdAt: {$gt: current.createdAt}}, {sort: {createdAt: 1}, limit:1});
+        //console.log(next);
+        //return next;
+
+        var postId = Router.current().params._id,
+            posts = Posts.find().fetch();
+
+        console.log(postId);
+
+        for (var i = 0; i < posts.length; ++i) {
+            if (posts[i]._id === postId) {
+                var current = posts[i],
+                    index = posts.indexOf(current);
+
+                index++;
+
+                var nextPost = Posts.findOne(posts[index]._id);
+                return nextPost;
+
+                //if (index === posts.length) {
+                //	e.preventDefault();
+                //} else {
+                //	var nextPost = Posts.findOne(posts[index]._id);
+                //}
+            } else {
+                e.preventDefault();
+            }
+        }
     }
 });
