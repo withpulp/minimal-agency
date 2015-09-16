6a5327da457f0a2e577d3e0eca18d330ed27e94f
Damir <damir@withpulp.com>
2015-09-15 20:49:41 -0400

    next post module wip

--- client/templates/modules/posts/post.js
+++ client/templates/modules/posts/post.js
@@ -1,30 +1,36 @@
+//var postId = Router.current().params._id,
+//	posts = Posts.find().fetch();
+//
+//console.log(postId);
+
+//for(var i = 0; i < posts.length; ++i) {
+//	if (posts[i]._id === postId) {
+//		var current = posts[i],
+//			index = posts.indexOf(current);
+//
+//		index++;
+//
+//		var nextPost = Posts.findOne(posts[index]._id);
+//
+//		//if (index === posts.length) {
+//		//	e.preventDefault();
+//		//} else {
+//		//	var nextPost = Posts.findOne(posts[index]._id);
+//		//}
+//	} else {
+//		e.preventDefault();
+//	}
+//}
+
 Template.post.helpers({
-	post: function() {
-		return Posts.findOne(Router.current().params._id);
-	},
-	next: function() {
-		//return Posts.findOne(Router.current().params._id);
-		var postId = Router.current().params._id,
-			posts = Posts.find().fetch();
+    post: function () {
+        return Posts.findOne(Router.current().params._id);
+    },
+    next: function () {
+        var current = Router.current().params._id;
+        console.log(current);
 
-		for(var i = 0; i < posts.length; ++i) {
-			if (posts[i]._id === postId) {
-				var current = posts[i],
-					index = posts.indexOf(current);
+        return Posts.find({createdAt: {$gt: current.createdAt}}, {sort: {createdAt: 1}, limit:1});
 
-				index++;
-
-				var nextPost = Posts.findOne(posts[index]._id);
-
-				//if (index === posts.length) {
-				//	e.preventDefault();
-				//} else {
-				//	var nextPost = Posts.findOne(posts[index]._id);
-				//}
-			} else {
-				e.preventDefault();
-			}
-		}
-		return nextPost;
-	}
+    }
 });
