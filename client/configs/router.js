a910575b0c94db0aee0f433cc8a2118164095fdd
Damir <damir@withpulp.com>
2015-10-04 05:10:42 -0400

    styled mobile menu

--- client/configs/router.js
+++ client/configs/router.js
@@ -5,3 +5,16 @@ Router.configure({
 });
 
 Router.onBeforeAction('loading');
+Router.onBeforeAction('dataNotFound', {only: 'case'});
+
+Router._filters = {
+  resetScroll: function () {
+    var scrollTo = window.currentScroll || 0;
+    $('body').scrollTop(scrollTo);
+  }
+};
+
+var filters = Router._filters;
+
+Router.onAfterAction(filters.resetScroll);
+
--- router/routes.js
+++ router/routes.js
@@ -302,18 +302,17 @@ Router.route('/blog/:_id', {
     SEO.set({title: Meteor.App.NAME});
 });
 
-Router.onBeforeAction('dataNotFound', {only: 'case'});
-
-Router._filters = {
-  resetScroll: function () {
-    var scrollTo = window.currentScroll || 0;
-    $('body').scrollTop(scrollTo);
-    $('body').css("min-height", 0);
-  }
-};
-
-var filters = Router._filters;
-
-if(Meteor.isClient) {
-  Router.onAfterAction(filters.resetScroll); // for all pages
-}
+//Router.onBeforeAction('dataNotFound', {only: 'case'});
+//
+//Router._filters = {
+//  resetScroll: function () {
+//    var scrollTo = window.currentScroll || 0;
+//    $('body').scrollTop(scrollTo);
+//  }
+//};
+//
+//var filters = Router._filters;
+//
+//if (Meteor.isClient) {
+//  Router.onAfterAction(filters.resetScroll);
+//}
