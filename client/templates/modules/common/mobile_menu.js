074a4e15cd02a8f883cd0f21861f22f949f679bd
Damir <damir@withpulp.com>
2015-10-04 05:11:41 -0400

    removed mobile_menu template

--- client/templates/modules/common/mobile_menu.js
+++
@@ -1,7 +0,0 @@
-Template['mobileMenu'].events({
-  'click .mobile-nav-trigger': function() {
-    var hello = console.log('hello');
-    Session.set('showMobileNav', hello);
-    Session.get('showMobileNav');
-  }
-});
